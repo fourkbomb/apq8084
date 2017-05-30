b24742b2ea4fa15219772917c14bb25da28dac1b
Simon Shields <simon@lineageos.org>
2017-05-30 18:29:41 +1000

    import N915GXXS1DPJ1 on top of LA.BF.2.1.2_rb1.7

--- net/socket.c
+++ net/socket.c
@@ -1290,6 +1290,8 @@ int __sock_create(struct net *net, int family, int type, int protocol,
 	}
 
 	sock->type = type;
+	sock->knox_uid = current->cred->uid;
+	sock->knox_pid = current->tgid;
 
 #ifdef CONFIG_MODULES
 	/* Attempt to load a protocol module if the find failed.
@@ -1794,6 +1796,8 @@ SYSCALL_DEFINE6(sendto, int, fd, void __user *, buff, size_t, len,
 
 	if (len > INT_MAX)
 		len = INT_MAX;
+	if (unlikely(!access_ok(VERIFY_READ, buff, len)))
+		return -EFAULT;
 	sock = sockfd_lookup_light(fd, &err, &fput_needed);
 	if (!sock)
 		goto out;
@@ -1853,6 +1857,8 @@ SYSCALL_DEFINE6(recvfrom, int, fd, void __user *, ubuf, size_t, size,
 
 	if (size > INT_MAX)
 		size = INT_MAX;
+	if (unlikely(!access_ok(VERIFY_WRITE, ubuf, size)))
+		return -EFAULT;
 	sock = sockfd_lookup_light(fd, &err, &fput_needed);
 	if (!sock)
 		goto out;
